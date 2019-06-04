3cb9ea39d0c3ba02a5667e11c76f40c766e9cccc
Sumit Saini <sumit.saini@nagarro.com>
2019-06-04 12:03:31 +0530

    final commit

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace SimpleAPI.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "dotnet", "playbook" };
+            return new string[] { "Timus", "Inias" };
         }
 
         // GET api/values/5
