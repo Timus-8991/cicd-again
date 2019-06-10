ad6bef0fc758e1ec0bed32ef402fffd68942eea9
Sumit Saini <sumit.saini@nagarro.com>
2019-06-10 13:31:08 +0530

    jfs

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace SimpleAPI.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "Timus", "Inias" };
+            return new string[] { "in", "api1" };
         }
 
         // GET api/values/5
